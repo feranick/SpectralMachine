20eac3f71c3667f5cd1e6b788c79b2689fe5b5f7
Nicola Ferralis <feranick@hotmail.com>
2016-09-15 13:56:09 -0400

    NewMaps are saved with Energy data in first row.

--- SVM_Maps/SVM_learning_spectra_selected.py
+++ SVM_Maps/SVM_learning_spectra_selected.py
@@ -5,7 +5,7 @@
 #
 # SVM_learning_spectra_selected
 # Perform SVM machine learning on Raman maps.
-# version: 20160915b
+# version: 20160915c
 #
 # By: Nicola Ferralis <feranick@hotmail.com>
 #
@@ -30,18 +30,16 @@ except:
         print(" Opening learning files and parameters...")
         f = open(mapfile, 'r')
         M = np.loadtxt(f, unpack =False)
-        #A = np.delete(A, np.s_[0:2], 1)
         f.close()
-        #print(M)
-        #print(M.shape)
-
+        
+        En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
+        
+        M = np.delete(M,np.s_[0:1],0)
         #Cl = [round(x*100) for x in M[:,0]]
         Cl = ['{:.2f}'.format(x) for x in M[:,0]]
         
-        #print(type(Cl[0]))
         A = np.delete(M,np.s_[0:1],1)
-        #print(A)
-        #print(Cl)
+
         print(' Retraining data...\n')
         clf = svm.SVC(kernel = kernel, C = 1.0)
         clf.fit(A,Cl)
--- SVM_Maps/SpectraExtractor.py
+++ SVM_Maps/SpectraExtractor.py
@@ -4,7 +4,7 @@
 #
 # SpectraExtractor.py
 # Extract spectra of specific phases
-# version: 20160915b
+# version: 20160915c
 #
 # By: Nicola Ferralis <feranick@hotmail.com>
 #
@@ -22,6 +22,10 @@ mapfile = "Dracken-7-tracky_map1_bs_fit2_despiked.txt"
 clustfile = "Draken_map1_fit3-den_ratio-d1g-col-clust-all.txt"
 newMapFile = "Dracken-7-tracky_map1_bs_fit2_selected.txt"
 
+with open(mapfile, 'r') as f:
+    En = np.array(f.readline().split(), dtype=np.dtype(float))
+
+
 f = open(mapfile, 'r')
 A = np.loadtxt(f, unpack =False, skiprows=1)
 A = np.delete(A, np.s_[0:2], 1)
@@ -38,14 +42,12 @@ L = np.loadtxt(f, unpack =False, skiprows=1, usecols = range(labelColumn-1,label
 f.close()
 print(' Shape label vector: ' + str(L.shape))
 
-phaseMap = np.zeros(A.shape[1]+1)
+phaseMap = np.append([0], En)
 
 for i in range(0,A.shape[0]):
     if Cl[i] == selPhase:
         temp = np.append(L[i], A[i,:])
         phaseMap = np.vstack((phaseMap, temp))
-
-phaseMap = np.delete(phaseMap, np.s_[0:1], 0)
 
 print(' Shape new map: ' + str(phaseMap.shape) + '\n')
 #print(phaseMap)
