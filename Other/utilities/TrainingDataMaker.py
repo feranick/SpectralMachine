3c7f7f81f790ab60fcd687e80b84505cdcd372cc
Nicola Ferralis <feranick@hotmail.com>
2017-03-01 13:15:03 -0500

    Refactored and simplified TrainingDataMaker

--- Other/utilities/TrainingDataMaker.py
+++ Other/utilities/TrainingDataMaker.py
@@ -23,11 +23,11 @@ import sys, os.path
 #**********************************************
 
 def main():
-    try:
-        makeFile(sys.argv[1], sys.argv[2], sys.argv[3])
-    except:
-        usage()
-    sys.exit(2)
+    #try:
+    makeFile(sys.argv[1], sys.argv[2], sys.argv[3])
+        #except:
+        #usage()
+#sys.exit(2)
 
 #**********************************************
 ''' Make Training file '''
@@ -49,7 +49,7 @@ def makeFile(trainFile, sampleFile, param):
     with open(sampleFile, 'r') as f:
         R = np.loadtxt(f, unpack = True, usecols=range(1,2))
 
-    try:
+    if os.path.exists(trainFile):
         with open(trainFile, 'r') as f:
             M = np.loadtxt(f, unpack =False)
             EnT = np.delete(np.array(M[0,:]),np.s_[0:1],0)
@@ -59,18 +59,13 @@ def makeFile(trainFile, sampleFile, param):
                 print('\033[1m' + ' Mismatch in datapoints: ' + str(EnT.shape[0]) + '; sample = ' +  str(En.shape[0]) + '\033[0m')
                 R = np.interp(EnT, En, R)
                 print('\033[1m' + ' Mismatch corrected: datapoints in sample: ' + str(R.shape[0]) + '\033[0m')
-    except:
-        print('\033[1m' + ' Train data file not found \n' + '\033[0m')
-        return
-
-
-    if os.path.exists(trainFile):
-        print('\n Adding spectra to ' + mapFile + '\n')
-        newTrain = np.append(float(param),R).reshape(1,-1)
+            print('\n Added spectra to ' + trainFile + '\n')
+            newTrain = np.append(float(param),R).reshape(1,-1)
     else:
-        print('\n' + trainFile + ' does not exist. Creating...' + '\n')
+        print('\n\033[1m' + ' Train data file not found. Creating...' + '\033[0m')
         newTrain = np.append([0], En)
-        newTrain = np.vstack((newMap, np.append(float(param),R)))
+        print(' Added spectra to ' + trainFile + '\n')
+        newTrain = np.vstack((newTrain, np.append(float(param),R)))
 
     with open(trainFile, 'ab') as f:
         np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
