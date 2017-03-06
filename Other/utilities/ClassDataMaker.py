9efab937ccb624028cf861fbea49a36150834444
Nicola Ferralis <feranick@hotmail.com>
2017-03-06 13:40:15 -0500

    Bug fixes and improvements on ClassDataMaker

--- Other/utilities/ClassDataMaker.py
+++ Other/utilities/ClassDataMaker.py
@@ -15,6 +15,7 @@ print(__doc__)
 
 import numpy as np
 import sys, os.path, glob
+from datetime import datetime, date
 
 #**********************************************
 ''' main '''
@@ -30,20 +31,27 @@ def main():
 #**********************************************
 ''' Open and process inividual files '''
 #**********************************************
-def processMultiFile(pcaFile):
+def processMultiFile(leearnFile):
     index = 0
+    learnFileRoot = os.path.splitext(learnFile)[0]
+    summary_filename = learnFileRoot + str(datetime.now().strftime('_%Y-%m-%d_%H-%M-%S.log'))
+    with open(summary_filename, "a") as sum_file:
+        sum_file.write(str(datetime.now().strftime('Classification started: %Y-%m-%d %H:%M:%S\n')))
     for f in glob.glob('*.txt'):
-        if (f != pcaFile):
-            makeFile(f, pcaFile, index)
+        if (f != learnFile):
+            makeFile(f, learnFile, index)
             index = index + 1
-    
+            with open(summary_filename, "a") as sum_file:
+                sum_file.write(str(index) + '\t' + f +'\n')
+
 #**********************************************
 ''' Add data to Training file '''
 #**********************************************
-def makeFile(sampleFile, pcaFile, param):
+def makeFile(sampleFile, learnFile, param):
     try:
         with open(sampleFile, 'r') as f:
-            En = np.loadtxt(f, unpack = True, usecols=range(0,1))
+            En = np.loadtxt(f, unpack = True, skiprows = 10, usecols=range(0,1))
+            print(En)
             print(' Number of points in \"' + sampleFile + '\": ' + str(En.shape[0]))
     except:
         print('\033[1m' + ' Sample data file not found \n' + '\033[0m')
@@ -52,25 +60,25 @@ def makeFile(sampleFile, pcaFile, param):
     with open(sampleFile, 'r') as f:
         R = np.loadtxt(f, unpack = True, usecols=range(1,2))
 
-    if os.path.exists(pcaFile):
-        with open(pcaFile, 'r') as f:
+    if os.path.exists(learnFile):
+        with open(learnFile, 'r') as f:
             M = np.loadtxt(f, unpack =False)
             EnT = np.delete(np.array(M[0,:]),np.s_[0:1],0)
             if EnT.shape[0] == En.shape[0]:
-                print(' Number of points in the pca dataset: ' + str(EnT.shape[0]))
+                print(' Number of points in the learning dataset: ' + str(EnT.shape[0]))
             else:
                 print('\033[1m' + ' Mismatch in datapoints: ' + str(EnT.shape[0]) + '; sample = ' +  str(En.shape[0]) + '\033[0m')
                 R = np.interp(EnT, En, R)
                 print('\033[1m' + ' Mismatch corrected: datapoints in sample: ' + str(R.shape[0]) + '\033[0m')
-            print('\n Added spectra to \"' + pcaFile + '\"\n')
+            print('\n Added spectra to \"' + learnFile + '\"\n')
             newTrain = np.append(float(param),R).reshape(1,-1)
     else:
         print('\n\033[1m' + ' Train data file not found. Creating...' + '\033[0m')
         newTrain = np.append([0], En)
-        print(' Added spectra to \"' + pcaFile + '\"\n')
+        print(' Added spectra to \"' + learnFile + '\"\n')
         newTrain = np.vstack((newTrain, np.append(float(param),R)))
 
-    with open(pcaFile, 'ab') as f:
+    with open(learnFile, 'ab') as f:
         np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
 
 #************************************
@@ -78,7 +86,7 @@ def makeFile(sampleFile, pcaFile, param):
 #************************************
 def usage():
     print('\n Usage:')
-    print('  python ClassDataMaker.py <pcafile>\n')
+    print('  python ClassDataMaker.py <learnfile>\n')
 
 #************************************
 ''' Main initialization routine '''
