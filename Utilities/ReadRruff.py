95bd37157d85b6494239995e47d72c01f5c43097
Nicola Ferralis <feranick@hotmail.com>
2017-06-12 12:16:58 -0400

    Fixed indenting and reading bugs in ReadRruff.py

--- Utilities/ReadRruff.py
+++ Utilities/ReadRruff.py
@@ -6,7 +6,7 @@
 * ReadRRuFF
 * Convert RRuFFspectra to ASCII
 * File must be in RRuFF
-* version: 20170526a
+* version: 201700612a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -19,25 +19,25 @@ import numpy as np
 import sys, os.path, getopt, glob, csv
 
 def main():
-	if(len(sys.argv)<2):
-		print(' Usage:\n  python3 readrruff.py <RRuFF filename>\n')
-		print(' Requires python 3.x. Not compatible with python 2.x\n')
-		return
+    if(len(sys.argv)<2):
+        print(' Usage:\n  python3 readrruff.py <RRuFF filename>\n')
+        print(' Requires python 3.x. Not compatible with python 2.x\n')
+        return
 
-	try:
-		with open(sys.argv[1], 'r') as f:
-			M = np.loadtxt(f, skiprows = 9, delimiter = ',', unpack=False)
-		print(str(' ' + sys.argv[1]) + '\n File OK, converting to ASCII... \n')
-	except:
-		print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
-		return
+    try:
+        with open(sys.argv[1], 'r') as f:
+            M = np.loadtxt(f, skiprows = 10, delimiter = ',', unpack=False)
+        print(str(' ' + sys.argv[1]) + '\n File OK, converting to ASCII... \n')
+    except:
+        print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
+        return
 
-	newFile = os.path.splitext(sys.argv[1])[0] + '_b.txt'
+    newFile = os.path.splitext(sys.argv[1])[0] + '_b.txt'
 
-	with open(newFile, 'ab') as f:
-		np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
+    with open(newFile, 'ab') as f:
+        np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
 
-	print(' Done!\n')
+    print(' Done!\n')
 
 #************************************
 ''' Main initialization routine '''
