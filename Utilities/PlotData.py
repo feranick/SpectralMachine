ceeba1ea3dd7f1fe00d48bc01443eff0bc95c9af
Nicola Ferralis <feranick@hotmail.com>
2017-10-12 17:18:46 -0400

    Improved plotting data utility

--- Utilities/PlotData.py
+++ Utilities/PlotData.py
@@ -3,9 +3,9 @@
 '''
 *********************************************
 *
-* Plot data
+* Plot train data
 *
-* version: 20170807a
+* version: 20171012a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -19,13 +19,17 @@ import sys, os.path, getopt, glob, csv
 
 def main():
     if(len(sys.argv)<2):
-        print(' Usage:\n  python3 PlotData.py <learnData>\n')
+        print(' Usage:\n  python3 PlotData.py <learnData> <step>\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
 
-    En, M = readLearnFile(sys.argv[1])
+    try:
+        step = int(sys.argv[2])
+    except:
+        step = 1
+    En, M, learnFileRoot = readLearnFile(sys.argv[1])
     
-    plotTrainData(En, M)
+    plotTrainData(En, M, learnFileRoot, step)
 
 #************************************
 ''' Open Learning Data '''
@@ -38,6 +42,8 @@ def readLearnFile(learnFile):
         print('\033[1m' + ' Learn data file not found \n' + '\033[0m')
         return
 
+    learnFileRoot = os.path.splitext(learnFile)[0]
+
     En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
     M = np.delete(M,np.s_[0:1],0)
     
@@ -45,16 +51,23 @@ def readLearnFile(learnFile):
     print("M:",M.shape)
     print ("En:",En)
     print("M:",M)
-    return En, M
+    return En, M, learnFileRoot
 
 #************************************
 ''' Plot data '''
 #************************************
-def plotTrainData(En, M):
+def plotTrainData(En, M, learnFileRoot, step):
     import matplotlib.pyplot as plt
 
     #print(' Plotting Training dataset in: ' + learnFileRoot + '.png\n')
-    for i in range(0,M.shape[0], 1):
+    if step == 1:
+        learnFileRoot = learnFileRoot + '_full-set'
+    else:
+        learnFileRoot = learnFileRoot + '_partial-' + str(step)
+
+    print(' Plotting Training dataset in: ' + learnFileRoot + '.png\n')
+    
+    for i in range(0,M.shape[0], step):
         plt.plot(En, M[i,1:], label='Training data')
     
     plt.plot(En, M[0,1:], label='Training data')
@@ -62,7 +75,7 @@ def plotTrainData(En, M):
     plt.xlabel('Raman shift [1/cm]')
     plt.ylabel('Raman Intensity [arb. units]')
 
-    #plt.savefig(learnFileRoot + '.png', dpi = 160, format = 'png')  # Save plot
+    plt.savefig(learnFileRoot + '.png', dpi = 160, format = 'png')  # Save plot
     
     plt.show()
     plt.close()
