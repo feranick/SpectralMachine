791b06c0e3d6aba983222a199e79ddddad5ea58c
Nicola Ferralis <feranick@hotmail.com>
2017-10-13 18:14:22 -0400

    Improvements on XRange

--- Utilities/XRange.py
+++ Utilities/XRange.py
@@ -3,7 +3,7 @@
 '''
 *********************************************
 * Change the Xrange
-* version: 20171013a
+* version: 20171013b
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 def main():
     if(len(sys.argv)<2):
-        print(' Usage:\n  python3 XRange.py <learnData> <min> <max>\n')
+        print(' Usage:\n  python3 XRange.py <learnData> <min x-axis> <max x-axis>\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
 
@@ -24,11 +24,11 @@ def main():
         min = int(sys.argv[2])
         max = int(sys.argv[3])
         En, M = resize(En, M, min, max)
+        learnFileRoot = learnFileRoot+"_range-"+str(min)+"-"+str(max)+".txt"
+        saveCvs(P,En,M,learnFileRoot)
+        plotTrainData(En, M, learnFileRoot)
     except:
-        pass
-    
-    plotTrainData(En, M, learnFileRoot)
-    saveCvs(P,En,M,learnFileRoot)
+        print("\n Training file not changed. Exit\n")
 
 #************************************
 ''' Open Learning Data '''
@@ -47,23 +47,29 @@ def readLearnFile(learnFile):
     En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
     M = np.delete(np.array(M[:,1:]),np.s_[0:1],0)
 
+    print("Param:", P.shape)
     print("En:",En.shape)
     print("M:",M.shape)
-    print("Param:", P.shape)
     #print ("En:",En)
     #print("M:",M)
     return En, M, P, learnFileRoot
 
+#************************************
+''' Resize Learning Data '''
+#************************************
 def resize(En, M, min, max):
-    En = En[min:(max)]
-    M = M[:,min:max]
-    print(En.shape)
-    print(M.shape)
+    indMin = np.where(En>min)[0][0]
+    indMax = np.where(En>max)[0][0]
+    En = En[indMin:indMax]
+    M = M[:,indMin:indMax]
+    print("New En:", En.shape)
+    print("New M:", M.shape)
     return En, M
 
-def saveCvs(P,En,M,learnFileRoot):
-    
-    trainFile = learnFileRoot+"_test.txt"
+#************************************
+''' Save New Learning Data '''
+#************************************
+def saveCvs(P,En,M,trainFile):
     newTrain = np.append([0], En)
     for i in range(0,M.shape[0]):
 
@@ -80,7 +86,7 @@ def plotTrainData(En, M, learnFileRoot):
 
     start = 0
     learnFileRootNew = learnFileRoot + '_full-set'
-    plt.title(learnFileRoot+'\nFull set (#'+str(M.shape[0])+')')
+    plt.title(learnFileRoot+'\nRestricted X Range')
 
     print(' Plotting Training dataset in: ' + learnFileRootNew + '.png\n')
     
@@ -92,7 +98,7 @@ def plotTrainData(En, M, learnFileRoot):
     plt.xlabel('Raman shift [1/cm]')
     plt.ylabel('Raman Intensity [arb. units]')
 
-    plt.savefig(learnFileRootNew + '.png', dpi = 160, format = 'png')  # Save plot
+    #plt.savefig(learnFileRootNew + '.png', dpi = 160, format = 'png')  # Save plot
     
     plt.show()
     plt.close()
