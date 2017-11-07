ef94754230f5c53fd9e526974046689b7c2b9f26
Nicola Ferralis <feranick@hotmail.com>
2017-11-06 23:09:49 -0500

    Bump to version 20171106a

--- SpectraLearnPredict/SpectraLearnPredict/slp/__init__.py
+++ SpectraLearnPredict/SpectraLearnPredict/slp/__init__.py
@@ -5,7 +5,7 @@
 *
 * SpectraLearnPredict
 * Perform Machine Learning on Spectroscopy Data.
-* version: 20171022a
+* version: 20171106a
 *
 * Uses: Deep Neural Networks, TensorFlow, SVM, PCA, K-Means
 *
