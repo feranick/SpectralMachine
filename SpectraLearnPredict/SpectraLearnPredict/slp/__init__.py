7ff3c02a3b82ac187f15a9c6adfc548c8fbb7635
Nicola Ferralis <feranick@hotmail.com>
2017-12-06 17:56:26 -0500

    Do not improve and rerun training in DNNClassifier during inference

--- SpectraLearnPredict/SpectraLearnPredict/slp/__init__.py
+++ SpectraLearnPredict/SpectraLearnPredict/slp/__init__.py
@@ -5,7 +5,7 @@
 *
 * SpectraLearnPredict
 * Perform Machine Learning on Spectroscopy Data.
-* version: 20171122a
+* version: 20171206a
 *
 * Uses: Deep Neural Networks, TensorFlow, SVM, PCA, K-Means
 *
