b709c17e9b55abfba0bf8baf7ec1120e20268e4d
Nicola Ferralis <feranick@hotmail.com>
2018-01-13 11:36:53 -0500

    Improved getting Rruff Raman data script

---
+++ Utilities/DownloadExtractZip.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# DownloadExtractZip - a script to extract Zips into folder
+#
+# By Nicola Ferralis <feranick@hotmail.com>
+#
+# This is script is licensed throughthe GNU Public license v.2.0
+#
+
+version="20180113a"
+if [ "$1" = "" -o "$1" = "-h" ]; then
+    echo
+    echo " ExtractZip v."$version
+    echo " Usage:"
+    echo "  extractzip.sh <destination folder>"
+    echo
+else
+    folder="rruff-raman_"$1
+    mkdir $folder
+    cd $folder
+    wget http://rruff.info/zipped_data_files/raman/LR-Raman.zip
+    wget http://rruff.info/zipped_data_files/raman/excellent_oriented.zip
+    wget http://rruff.info/zipped_data_files/raman/excellent_unoriented.zip
+    wget http://rruff.info/zipped_data_files/raman/fair_oriented.zip
+    wget http://rruff.info/zipped_data_files/raman/fair_unoriented.zip
+    wget http://rruff.info/zipped_data_files/raman/ignore_unoriented.zip
+    wget http://rruff.info/zipped_data_files/raman/poor_oriented.zip
+    wget http://rruff.info/zipped_data_files/raman/poor_unoriented.zip
+    wget http://rruff.info/zipped_data_files/raman/unrated_oriented.zip
+    wget http://rruff.info/zipped_data_files/raman/unrated_unoriented.zip
+    echo " Create folder: "$1
+    mkdir $1
+    pathfiles=./
+    for i in $( ls $pathfiles );
+        do
+        if [ "${i##*.}" = "zip" ]; then
+            unzip $i -d $1
+        fi
+    echo
+    echo " Done!"
+    echo
+    done
+fi
+
