d8ccc5cab3b0f2a66399f54ed47ff2cfdd8a3357
Nicola Ferralis <feranick@hotmail.com>
2018-06-15 16:27:23 -0400

    Added HDF5 support for AddLinearBackground.py

--- Utilities/AddLinearBackground.py
+++ Utilities/AddLinearBackground.py
@@ -8,7 +8,7 @@
 * slope parameter.
 * For augmentation of data
 *
-* version: 20171218a
+* version: 20180615a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -16,11 +16,13 @@
 '''
 print(__doc__)
 
-
 import numpy as np
-import sys, os.path, getopt, glob
-
+import sys, os.path, h5py
+#************************************
+''' Main '''
+#************************************
 class defParam:
+    saveAsTxt = False
     addToFlatland = False
 
 def main():
@@ -51,25 +53,45 @@ def main():
     for j in range(int(sys.argv[2])):
         newTrain = np.vstack((newTrain, linBackground(En, M, float(sys.argv[3]))))
 
-    with open(newFile, 'ab') as f:
-        np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
-
-    print(' New training file saved:', newFile, '\n')
+    saveLearnFile(newTrain, newFile)
 
 #************************************
 ''' Open Learning Data '''
 #************************************
 def readLearnFile(learnFile):
+    print(" Opening learning file: "+learnFile+"\n")
     try:
-        with open(learnFile, 'r') as f:
-            M = np.loadtxt(f, unpack =False)
+        if os.path.splitext(learnFile)[1] == ".npy":
+            M = np.load(learnFile)
+        elif os.path.splitext(learnFile)[1] == ".h5":
+            with h5py.File(learnFile, 'r') as hf:
+                M = hf["M"][:]
+        else:
+            with open(learnFile, 'r') as f:
+                M = np.loadtxt(f, unpack =False)
     except:
-        print('\033[1m' + ' Learn data file not found \n' + '\033[0m')
+        print("\033[1m" + " Learning file not found \n" + "\033[0m")
         return
 
-    En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
-    M = np.delete(M,np.s_[0:1],0)
-    return En, M
+    En = M[0,1:]
+    A = M[1:,:]
+    #Cl = M[1:,0]
+    return En, A
+
+#***************************************
+''' Save new learning Data '''
+#***************************************
+def saveLearnFile(M, learnFile):
+    if defParam.saveAsTxt == True:
+        learnFile += '.txt'
+        print(" Saving new training file (txt) in:", learnFile+"\n")
+        with open(learnFile, 'ab') as f:
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
+    else:
+        learnFile += '.h5'
+        print(" Saving new training file (hdf5) in: "+learnFile+"\n")
+        with h5py.File(learnFile, 'w') as hf:
+            hf.create_dataset("M",  data=M)
 
 #************************************
 ''' Introduce Noise in Data '''
