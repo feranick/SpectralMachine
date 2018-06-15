b2c19ca40572bfc0ed6a2a49eb69e4b3591ca49e
Nicola Ferralis <feranick@hotmail.com>
2018-06-15 16:22:07 -0400

    Added HDF5 support for AddVerticalOffset.py

--- Utilities/AddVerticalOffset.py
+++ Utilities/AddVerticalOffset.py
@@ -6,7 +6,7 @@
 * Replicate training data with vertical offset
 * For augmentation of data
 *
-* version: 20170809b
+* version: 20180615a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -14,9 +14,13 @@
 '''
 print(__doc__)
 
-
 import numpy as np
-import sys, os.path, getopt, glob, csv
+import sys, os.path, csv, h5py
+#************************************
+''' Main '''
+#************************************
+class defParam:
+    saveAsTxt = False
 
 def main():
     if(len(sys.argv)<4):
@@ -25,7 +29,7 @@ def main():
         return
 
     En, M = readLearnFile(sys.argv[1])
-    newFile = os.path.splitext(sys.argv[1])[0] + '_n' + sys.argv[2]+ '_oV' + sys.argv[3] + '.txt'
+    newFile = os.path.splitext(sys.argv[1])[0] + '_n' + sys.argv[2]+ '_oV' + sys.argv[3]
 
     if os.path.exists(newFile) == False:
         newTrain = np.append([0], En)
@@ -38,25 +42,45 @@ def main():
     for j in range(int(sys.argv[2])):
         newTrain = np.vstack((newTrain, verticalOffset(M, float(sys.argv[3]))))
 
-    with open(newFile, 'ab') as f:
-        np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
-
-    print(' New training file saved: ', newFile, '\n')
+    saveLearnFile(newTrain, newFile)
 
 #************************************
 ''' Open Learning Data '''
 #************************************
 def readLearnFile(learnFile):
+    print(" Opening learning file: "+learnFile+"\n")
     try:
-        with open(learnFile, 'r') as f:
-            M = np.loadtxt(f, unpack =False)
+        if os.path.splitext(learnFile)[1] == ".npy":
+            M = np.load(learnFile)
+        elif os.path.splitext(learnFile)[1] == ".h5":
+            with h5py.File(learnFile, 'r') as hf:
+                M = hf["M"][:]
+        else:
+            with open(learnFile, 'r') as f:
+                M = np.loadtxt(f, unpack =False)
     except:
-        print('\033[1m' + ' Learn data file not found \n' + '\033[0m')
+        print("\033[1m" + " Learning file not found \n" + "\033[0m")
         return
 
-    En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
-    M = np.delete(M,np.s_[0:1],0)
-    return En, M
+    En = M[0,1:]
+    A = M[1:,:]
+    #Cl = M[1:,0]
+    return En, A
+
+#***************************************
+''' Save new learning Data '''
+#***************************************
+def saveLearnFile(M, learnFile):
+    if defParam.saveAsTxt == True:
+        learnFile += '.txt'
+        print(" Saving new training file (txt) in:", learnFile+"\n")
+        with open(learnFile, 'ab') as f:
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
+    else:
+        learnFile += '.h5'
+        print(" Saving new training file (hdf5) in: "+learnFile+"\n")
+        with h5py.File(learnFile, 'w') as hf:
+            hf.create_dataset("M",  data=M)
 
 #************************************
 ''' Introduce Noise in Data '''
@@ -64,7 +88,6 @@ def readLearnFile(learnFile):
 def verticalOffset(M, offset):    
     for i in range(1, M.shape[1]-1):
         M[:,i] += offset
-    
     return M
 
 #************************************
