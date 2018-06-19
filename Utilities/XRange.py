4379c94b4de6b1896af9fab0711177be11fc235c
Nicola Ferralis <feranick@hotmail.com>
2018-06-19 15:18:23 -0400

    Added HD%s support for XRance.py

--- Utilities/XRange.py
+++ Utilities/XRange.py
@@ -3,15 +3,21 @@
 '''
 *********************************************
 * Change the Xrange
-* version: 20171013b
+* version: 20180619a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************
 '''
 print(__doc__)
 import numpy as np
-import sys, os.path, random
+import sys, os.path, random, h5py
 import matplotlib.pyplot as plt
+
+#************************************
+''' Main '''
+#************************************
+class defParam:
+    saveAsTxt = False
 
 def main():
     if(len(sys.argv)<2):
@@ -19,13 +25,13 @@ def main():
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
 
-    En, M, P, learnFileRoot = readLearnFile(sys.argv[1])
+    En, Cl, M, learnFileRoot = readLearnFile(sys.argv[1])
     try:
         min = int(sys.argv[2])
         max = int(sys.argv[3])
         En, M = resize(En, M, min, max)
         learnFileRoot = learnFileRoot+"_range-"+str(min)+"-"+str(max)+".txt"
-        saveCvs(P,En,M,learnFileRoot)
+        saveNewLearnFile(En,Cl,M,learnFileRoot)
         plotTrainData(En, M, learnFileRoot)
     except:
         print("\n Training file not changed. Exit\n")
@@ -34,25 +40,28 @@ def main():
 ''' Open Learning Data '''
 #************************************
 def readLearnFile(learnFile):
+    print(" Opening learning file: "+learnFile+"\n")
     try:
-        with open(learnFile, 'r') as f:
-            M = np.loadtxt(f, unpack =False)
+        if os.path.splitext(learnFile)[1] == ".npy":
+            M = np.load(learnFile)
+        elif os.path.splitext(learnFile)[1] == ".h5":
+            with h5py.File(learnFile, 'r') as hf:
+                M = hf["M"][:]
+        else:
+            with open(learnFile, 'r') as f:
+                M = np.loadtxt(f, unpack =False)
     except:
-        print('\033[1m' + ' Learn data file not found \n' + '\033[0m')
+        print("\033[1m" + " Learning file not found \n" + "\033[0m")
         return
-
+    
     learnFileRoot = os.path.splitext(learnFile)[0]
-
-    P = M[1:,0]
-    En = np.delete(np.array(M[0,:]),np.s_[0:1],0)
-    M = np.delete(np.array(M[:,1:]),np.s_[0:1],0)
-
-    print("Param:", P.shape)
+    En = M[0,1:]
+    A = M[1:,1:]
+    Cl = M[1:,0]
+    print("Classes:", Cl.shape)
     print("En:",En.shape)
     print("M:",M.shape)
-    #print ("En:",En)
-    #print("M:",M)
-    return En, M, P, learnFileRoot
+    return En, Cl, A, learnFileRoot
 
 #************************************
 ''' Resize Learning Data '''
@@ -69,14 +78,22 @@ def resize(En, M, min, max):
 #************************************
 ''' Save New Learning Data '''
 #************************************
-def saveCvs(P,En,M,trainFile):
+def saveNewLearnFile(En,Cl,M,learnFile):
     newTrain = np.append([0], En)
+    
     for i in range(0,M.shape[0]):
+        newTrain = np.vstack((newTrain, np.append(float(Cl[i]),M[i,:])))
 
-        newTrain = np.vstack((newTrain, np.append(float(P[i]),M[i,:])))
-
-    with open(trainFile, 'ab') as f:
-        np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
+    if defParam.saveAsTxt == True:
+        learnFile += '.txt'
+        print(" Saving new training file (txt) in:", learnFile+"\n")
+        with open(learnFile, 'ab') as f:
+            np.savetxt(f, newTrain, delimiter='\t', fmt='%10.6f')
+    else:
+        learnFile += '.h5'
+        print(" Saving new training file (hdf5) in: "+learnFile+"\n")
+        with h5py.File(learnFile, 'w') as hf:
+            hf.create_dataset("M",  data=newTrain)
 
 #************************************
 ''' Plot data '''
@@ -97,12 +114,9 @@ def plotTrainData(En, M, learnFileRoot):
 
     plt.xlabel('Raman shift [1/cm]')
     plt.ylabel('Raman Intensity [arb. units]')
-
     #plt.savefig(learnFileRootNew + '.png', dpi = 160, format = 'png')  # Save plot
-    
     plt.show()
     plt.close()
-
 
 #************************************
 ''' Main initialization routine '''
