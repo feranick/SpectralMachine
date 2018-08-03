53af8ac16d4e4d6cd9dd5a1a823aa41d829aaa41
Nicola Ferralis <feranick@hotmail.com>
2018-08-03 16:30:59 -0400

    New Utility: remode data from learning set if it has insufficient number of spectra per class
    This is based on a specific threshold.

---
+++ Utilities/RemoveLimitedDatasets.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+*********************************************
+*
+* RemoveLimitedDatasets
+* Remove data with little representation based on threshold
+*
+* version: 20180803a
+*
+* By: Nicola Ferralis <feranick@hotmail.com>
+*
+***********************************************
+'''
+print(__doc__)
+
+import numpy as np
+import sys, os.path, h5py
+#************************************
+''' Main '''
+#************************************
+class defParam:
+    saveAsTxt = True
+    addToFlatland = False
+    Ynorm = True
+    YnormTo = 1
+
+def main():
+    if len(sys.argv) < 3:
+        print(' Usage:\n  python3 RemoveLimitedDatasets.py <learnData> <threshold>')
+        print(' Requires python 3.x. Not compatible with python 2.x\n')
+        return
+
+    newFile = os.path.splitext(sys.argv[1])[0] + '_above' + sys.argv[2]
+    learnFileExt = os.path.splitext(sys.argv[1])[1]
+
+    '''
+    if learnFileExt == ".h5":
+        defParam.saveAsTxt = False
+    else:
+        defParam.saveAsTxt = True
+    '''
+
+    En, M = readLearnFile(sys.argv[1])
+
+    print(En.shape)
+    print(M.shape)
+    newTrain = np.append([0], En)
+    
+    
+    num = 0
+    ind = 0
+    tempTrain = M[0,:]
+
+    exclIndex = []
+
+    print(tempTrain.shape)
+    for i in range(M.shape[0]):
+        #print("initial: ", M[i,0], num)
+        if M[i,0] != ind or i == M.shape[0]-1:
+            if num >= float(sys.argv[2]):
+                print("Class: ",ind, "- number per class: ", num)
+                newTrain = np.vstack((newTrain,tempTrain))
+            else:
+                print("Class: ",ind, "- number per class: ", num, " EXCLUDED")
+                exclIndex.append(ind)
+            tempTrain = M[i,:]
+            ind = M[i,0]
+            num = 1
+        else:
+            tempTrain=np.vstack((tempTrain,M[i,:]))
+            num +=1
+            
+    saveLearnFile(newTrain, newFile)
+
+    print(exclIndex)
+    
+#************************************
+''' Open Learning Data '''
+#************************************
+def readLearnFile(learnFile):
+    print(" Opening learning file: "+learnFile+"\n")
+    try:
+        if os.path.splitext(learnFile)[1] == ".npy":
+            M = np.load(learnFile)
+        elif os.path.splitext(learnFile)[1] == ".h5":
+            with h5py.File(learnFile, 'r') as hf:
+                M = hf["M"][:]
+        else:
+            with open(learnFile, 'r') as f:
+                M = np.loadtxt(f, unpack =False)
+    except:
+        print("\033[1m" + " Learning file not found \n" + "\033[0m")
+        return
+
+    En = M[0,1:]
+    A = M[1:,:]
+    #Cl = M[1:,0]
+    return En, A
+
+#***************************************
+''' Save new learning Data '''
+#***************************************
+def saveLearnFile(M, learnFile):
+    if defParam.saveAsTxt == True:
+        learnFile += '.txt'
+        print("\n Saving new training file (txt) in:", learnFile+"\n")
+        with open(learnFile, 'ab') as f:
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
+    else:
+        learnFile += '.h5'
+        print("\n Saving new training file (hdf5) in: "+learnFile+"\n")
+        with h5py.File(learnFile, 'w') as hf:
+            hf.create_dataset("M",  data=M)
+
+
+#************************************
+''' Main initialization routine '''
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
