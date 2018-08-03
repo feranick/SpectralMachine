efa81cbec90fa16080969d958b82b7a5b95c38bd
Nicola Ferralis <feranick@hotmail.com>
2018-08-03 17:33:11 -0400

    Save also the excluded dataset in RemoveLimitedDatasets

--- Utilities/RemoveLimitedDatasets.py
+++ Utilities/RemoveLimitedDatasets.py
@@ -6,7 +6,7 @@
 * RemoveLimitedDatasets
 * Remove data with little representation based on threshold
 *
-* version: 20180803a
+* version: 20180803b
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -32,6 +32,7 @@ def main():
         return
 
     newFile = os.path.splitext(sys.argv[1])[0] + '_above' + sys.argv[2]
+    exclFile = os.path.splitext(sys.argv[1])[0] + '_excludeBelow' + sys.argv[2]
     learnFileExt = os.path.splitext(sys.argv[1])[1]
 
     '''
@@ -42,39 +43,35 @@ def main():
     '''
 
     En, M = readLearnFile(sys.argv[1])
-
-    print(En.shape)
-    print(M.shape)
     newTrain = np.append([0], En)
-    
-    
+    exclTrain = np.append([0], En)
+
     num = 0
     ind = 0
     tempTrain = M[0,:]
-
     exclIndex = []
 
-    print(tempTrain.shape)
     for i in range(M.shape[0]):
         #print("initial: ", M[i,0], num)
         if M[i,0] != ind or i == M.shape[0]-1:
             if num >= float(sys.argv[2]):
-                print("Class: ",ind, "- number per class: ", num)
+                print(" Class: ",ind, "- number per class: ", num)
                 newTrain = np.vstack((newTrain,tempTrain))
             else:
-                print("Class: ",ind, "- number per class: ", num, " EXCLUDED")
+                print(" Class: ",ind, "- number per class: ", num, " EXCLUDED")
                 exclIndex.append(ind)
+                exclTrain = np.vstack((exclTrain,tempTrain))
             tempTrain = M[i,:]
             ind = M[i,0]
             num = 1
         else:
             tempTrain=np.vstack((tempTrain,M[i,:]))
             num +=1
-            
-    saveLearnFile(newTrain, newFile)
+    print("\n")
 
-    print(exclIndex)
-    
+    saveLearnFile(newTrain, newFile)
+    saveLearnFile(exclTrain, exclFile)
+
 #************************************
 ''' Open Learning Data '''
 #************************************
@@ -104,15 +101,14 @@ def readLearnFile(learnFile):
 def saveLearnFile(M, learnFile):
     if defParam.saveAsTxt == True:
         learnFile += '.txt'
-        print("\n Saving new training file (txt) in:", learnFile+"\n")
+        print(" Saving new training file (txt) in:", learnFile+"\n")
         with open(learnFile, 'ab') as f:
             np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
     else:
         learnFile += '.h5'
-        print("\n Saving new training file (hdf5) in: "+learnFile+"\n")
+        print(" Saving new training file (hdf5) in: "+learnFile+"\n")
         with h5py.File(learnFile, 'w') as hf:
             hf.create_dataset("M",  data=M)
-
 
 #************************************
 ''' Main initialization routine '''
