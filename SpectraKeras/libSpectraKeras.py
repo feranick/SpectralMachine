5c6e78e88c2021775f48a8b37aa98b9190d59fdf
Nicola Ferralis <feranick@hotmail.com>
2018-11-05 18:11:09 -0500

    Add libSpectraKeras

---
+++ SpectraKeras/libSpectraKeras.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+'''
+**********************************************************
+* libDataML - Library for DataML
+* 20181102c
+* Uses: Keras, TensorFlow
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************************
+'''
+import numpy as np
+import pickle
+from bisect import bisect_left
+
+#************************************
+# Normalizer
+#************************************
+class Normalizer(object):
+    def __init__(self, M, dP):
+        self.M = M
+        self.normalizeLabel = dP.normalizeLabel
+        self.useGeneralNormLabel = dP.useGeneralNormLabel
+        self.useCustomRound = dP.useCustomRound
+        self.minGeneralLabel = dP.minGeneralLabel
+        self.maxGeneralLabel = dP.maxGeneralLabel
+        self.YnormTo = dP.YnormTo
+        self.stepNormLabel = dP.stepNormLabel
+        
+        self.data = np.arange(0,1,self.stepNormLabel)
+        self.min = np.zeros([self.M.shape[1]])
+        self.max = np.zeros([self.M.shape[1]])
+    
+        if self.normalizeLabel:
+            if self.useGeneralNormLabel:
+                self.min[0] = dP.minGeneralLabel
+                self.max[0] = dP.maxGeneralLabel
+            else:
+                self.min[0] = np.amin(self.M[1:,0])
+                self.max[0] = np.amax(self.M[1:,0])
+        
+        for i in range(1,M.shape[1]):
+            self.min[i] = np.amin(self.M[1:,i])
+            self.max[i] = np.amax(self.M[1:,i])
+    
+    def transform_matrix(self,y):
+        Mn = np.copy(y)
+        if self.normalizeLabel:
+            Mn[1:,0] = np.multiply(y[1:,0] - self.min[0], self.YnormTo/(self.max[0] - 
+self.min[0]))
+            if self.useCustomRound:
+                customData = CustomRound(self.data)
+                for i in range(1,y.shape[0]):
+                    Mn[i,0] = customData(Mn[i,0])
+
+        for i in range(1,y.shape[1]):
+            Mn[1:,i] = np.multiply(y[1:,i] - self.min[i], self.YnormTo/(self.max[i] - 
+self.min[i]))
+        return Mn
+        
+    def transform_valid(self,V):
+        Vn = np.copy(V)
+        for i in range(0,V.shape[0]):
+            Vn[i,1] = np.multiply(V[i,1] - self.min[i], self.YnormTo/(self.max[i] - 
+self.min[i]))
+        return Vn
+    
+    def transform_inverse_single(self,v):
+        vn = self.min[0] + v*(self.max[0] - self.min[0])/self.YnormTo
+        return vn
+
+    def save(self, name):
+        with open(name, 'ab') as f:
+            f.write(pickle.dumps(self))
+
+#************************************
+# CustomRound
+#************************************
+class CustomRound:
+    def __init__(self,iterable):
+        self.data = sorted(iterable)
+
+    def __call__(self,x):
+        data = self.data
+        ndata = len(data)
+        idx = bisect_left(data,x)
+        if idx <= 0:
+            return data[0]
+        elif idx >= ndata:
+            return data[ndata-1]
+        x0 = data[idx-1]
+        x1 = data[idx]
+        if abs(x-x0) < abs(x-x1):
+            return x0
+        return x1
