a5c8c9923b78b37ea0784c83fd621cedca5aab38
Nicola Ferralis <feranick@hotmail.com>
2018-11-20 16:40:51 -0500

    Fixed typo

--- SpectraKeras/SpectraKeras_MLP.py
+++ SpectraKeras/SpectraKeras_MLP.py
@@ -114,8 +114,8 @@ def main():
                 else:
                     train(sys.argv[2], sys.argv[3])
             except:
-               usage()
-              sys.exit(2)
+                usage()
+                sys.exit(2)
 
         if o in ("-p" , "--predict"):
             try:
