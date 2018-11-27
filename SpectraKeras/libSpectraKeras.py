babc295aa23e53fe08bd8da2a89b3401999ecd26
Nicola Ferralis <feranick@hotmail.com>
2018-11-27 17:05:42 -0500

    Fixed typo

--- SpectraKeras/libSpectraKeras.py
+++ SpectraKeras/libSpectraKeras.py
@@ -2,7 +2,7 @@
 '''
 **********************************************************
 * libSpectraKeas - Library for SpectraKeras
-* 20181126a
+* 20181127a
 * Uses: Keras, TensorFlow
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************************
