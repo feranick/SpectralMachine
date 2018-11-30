ddbc6a3e83f9a44e45ff0fa9ebdd050fa03d1323
Nicola Ferralis <feranick@hotmail.com>
2018-11-30 17:32:09 -0500

    Disable debugging

--- SpectraKeras/SpectraKeras_CNN.py
+++ SpectraKeras/SpectraKeras_CNN.py
@@ -128,28 +128,28 @@ def main():
 
     for o, a in opts:
         if o in ("-t" , "--train"):
-            #try:
-            if len(sys.argv)<4:
-                train(sys.argv[2], None)
-            else:
-                train(sys.argv[2], sys.argv[3])
-            #except:
-            #    usage()
-            #    sys.exit(2)
+            try:
+                if len(sys.argv)<4:
+                    train(sys.argv[2], None)
+                else:
+                    train(sys.argv[2], sys.argv[3])
+            except:
+                usage()
+                sys.exit(2)
 
         if o in ("-p" , "--predict"):
-            #try:
-            predict(sys.argv[2])
-            #except:
-            #    usage()
-            #    sys.exit(2)
+            try:
+                predict(sys.argv[2])
+            except:
+                usage()
+                sys.exit(2)
                 
         if o in ("-b" , "--batch"):
-            #try:
-            batchPredict()
-            #except:
-            #    usage()
-            #    sys.exit(2)
+            try:
+                batchPredict()
+            except:
+                usage()
+                sys.exit(2)
 
     total_time = time.clock() - start_time
     print(" Total time: {0:.1f}s or {1:.1f}m or {2:.1f}h".format(total_time,
