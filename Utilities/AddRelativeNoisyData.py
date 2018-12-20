135c4bb91bc6755e19903c4c00ed1492a2f87c30
Nicola Ferralis <feranick@hotmail.com>
2018-12-20 11:24:11 -0500

    Rewritten AddRelativeNoisyData.py

--- Utilities/AddRelativeNoisyData.py
+++ Utilities/AddRelativeNoisyData.py
@@ -7,7 +7,7 @@
 * Noise is a percentage of max in a spectra.
 * For augmentation of data
 *
-* version: 20180615a
+* version: 20181220a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -23,16 +23,27 @@ import sys, os.path, h5py
 class defParam:
     saveAsTxt = False
     addToFlatland = False
+    Ynorm = True
+    YnormTo = 1
 
 def main():
     if len(sys.argv) < 4:
         print(' Usage:\n  python3 AddRelativeNoisyData.py <learnData> <#additions> <%-offset>')
-        print('  Data is NOT normalized.\n')
+        print('  Data is by default normalized to 1\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
-    
+
+    newFile = os.path.splitext(sys.argv[1])[0] + '_n' + sys.argv[2] + '_offs' + sys.argv[3]
     newFile = os.path.splitext(sys.argv[1])[0] + '_n' + sys.argv[2] + '_oNpc' + sys.argv[3]
-    
+    learnFileExt = os.path.splitext(sys.argv[1])[1]
+
+    '''
+    if learnFileExt == ".h5":
+        defParam.saveAsTxt = False
+    else:
+        defParam.saveAsTxt = True
+    '''
+
     if len(sys.argv) == 5:
         defParam.addToFlatland = True
         newFile += '_back'
@@ -41,6 +52,11 @@ def main():
         print(' Adding', sys.argv[2], 'sets with random noise with offset:', sys.argv[3], '\n')
 
     En, M = readLearnFile(sys.argv[1])
+    
+    if defParam.Ynorm ==True:
+        print(" Normalizing Learning Spectra to:",defParam.YnormTo)
+        M = normalizeSpectra(M)
+        newFile += '_norm1'
 
     if os.path.exists(newFile) == False:
         newTrain = np.append([0], En)
@@ -50,6 +66,10 @@ def main():
 
     for j in range(int(sys.argv[2])):
         newTrain = np.vstack((newTrain, scrambleNoise(M, float(sys.argv[3]))))
+
+    if defParam.Ynorm ==True:
+        print(" Normalizing Learning + Noisy Spectra to:",defParam.YnormTo,"\n")
+        newTrain = normalizeSpectra(newTrain)
 
     saveLearnFile(newTrain, newFile)
 
@@ -97,12 +117,24 @@ def saveLearnFile(M, learnFile):
 def scrambleNoise(M, offset):
     from random import uniform
     for j in range(0, M.shape[0]):
-        for i in range(1, M.shape[1]):
+        for i in range(1, M.shape[1]-1):
             if defParam.addToFlatland == False:
-                M[j,i] += offset*uniform(-1,1)*np.amax(M[j,:])
+                #M[:,i] += 0.01*offset*uniform(-1,1)
+                M[j,i] *= abs(0.01*offset*uniform(-1,1)*np.amax(M[j,:]))
             else:
-                if M[j,i].any() == 0:
-                    M[j,i] += offset*uniform(-1,1)*np.amax(M[j,:])
+                if M[:,i].any() == 0:
+                    #M[:,i] += 0.01*offset*uniform(-1,1)
+                    M[j,i] *= abs(0.01*offset*uniform(-1,1)*np.amax(M[j,:]))
+    return M
+
+#************************************
+''' Normalize '''
+#************************************
+def normalizeSpectra(M):
+    for i in range(1,M.shape[0]):
+        if(np.amin(M[i]) <= 0):
+            M[i,1:] = M[i,1:] - np.amin(M[i,1:]) + 1e-8
+        M[i,1:] = np.multiply(M[i,1:], defParam.YnormTo/max(M[i][1:]))
     return M
 
 #************************************
