0b52f4ec57e0823af472cc3f93a46184a3a27b5d
Nicola Ferralis <feranick@hotmail.com>
2019-10-25 13:37:21 -0400

    ReadRruff can now handle batch and single file conversion and specific folder for output

--- Utilities/ReadRruff.py
+++ Utilities/ReadRruff.py
@@ -3,10 +3,10 @@
 '''
 *********************************************
 *
-* ReadRRuFF
+* ReadRruFF
 * Convert RRuFFspectra to ASCII
 * File must be in RRuFF
-* version: 201700612a
+* version: 20191025a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -17,29 +17,46 @@ print(__doc__)
 import numpy as np
 import sys, os.path, getopt, glob, csv
 
+#************************************
+# Main
+#************************************
 def main():
-    if(len(sys.argv)<2):
-        print(' Usage:\n  python3 readrruff.py <RRuFF filename>\n')
+    
+    if len(sys.argv) < 2:
+        print(' Usage:\n')
+        print(' (Single File): python3 ReadRruff.py <folder> <RRuFF filename>\n')
+        print(' (Batch conversion): python3 ReadRruff.py <folder>\n')
+        print(' <folder> Directory where ASCII files are to be saved\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
+    
+    if len(sys.argv) < 3:
+        for ind, f in enumerate(sorted(os.listdir("."))):
+            if (os.path.splitext(f)[-1] == ".txt"):
+                saveFile(sys.argv[1],f)
+    else:
+        saveFile(sys.argv[1],sys.argv[2])
 
+    print(' Done!\n')
+    
+#************************************
+# Save File
+#************************************
+def saveFile(folder, file):
+    print(file)
     try:
-        with open(sys.argv[1], 'r') as f:
+        with open(file, 'r') as f:
             M = np.loadtxt(f, skiprows = 10, delimiter = ',', unpack=False)
-        print(str(' ' + sys.argv[1]) + '\n File OK, converting to ASCII... \n')
+        print(str(' ' + file) + '\n File OK, converting to ASCII... \n')
+        newFile = folder+os.path.splitext(file)[0] + '_ASCII.txt'
+        with open(newFile, 'ab') as f:
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
     except:
         print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
         return
 
-    newFile = os.path.splitext(sys.argv[1])[0] + '_b.txt'
-
-    with open(newFile, 'ab') as f:
-        np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
-
-    print(' Done!\n')
-
 #************************************
-''' Main initialization routine '''
+# Main initialization routine
 #************************************
 if __name__ == "__main__":
     sys.exit(main())
