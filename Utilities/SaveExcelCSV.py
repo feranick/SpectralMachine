969afa093f531174d9d7af9353e67bcd9a5d518c
Nicola Ferralis <feranick@hotmail.com>
2021-06-11 19:02:52 -0400

    New utility: Save Excel files into CSV or H5

---
+++ Utilities/SaveExcelCSV.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+'''
+*********************************************
+* SaveExcelCSV
+* Adds data from single file to Master Doc
+* version: 20210611a
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************
+'''
+print(__doc__)
+import pandas as pd
+import sys, os.path, h5py
+
+#************************************
+# Parameters definition
+#************************************
+class dP:
+    saveAsCsv = True
+    saveAsHDF = True
+
+#************************************
+# Main
+#************************************
+def main():
+    if len(sys.argv) < 2:
+        print(' Usage:\n  python3 SaveExcelCSV.py <Excel File>')
+        print(' Requires python 3.x. Not compatible with python 2.x\n')
+        return
+    
+    root = os.path.splitext(sys.argv[1])[0]
+    print(" Opening Excel File:",sys.argv[1],"...\n")
+    ws = pd.read_excel(sys.argv[1], sheet_name=None)
+
+    for name, key in ws.items():
+        #print(name)
+        #print(ws[name])
+        if dP.saveAsCsv:
+            CSVfile = root+'_'+name+".csv"
+            print(" Sheet:",name,"saved in:",CSVfile)
+            ws[name].to_csv(CSVfile)
+        if dP.saveAsHDF:
+            HDFfile = root+'_'+name+".h5"
+            print(" Sheet:",name,"saved in:",HDFfile)
+            ws[name].to_hdf(HDFfile, key='df', mode='w')
+    print("\n")
+
+#************************************
+# Main initialization routine
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
