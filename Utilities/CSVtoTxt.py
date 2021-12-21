660b54a8d3d763e6d4b7ce2ad34da300848a3603
Nicola Ferralis <feranick@hotmail.com>
2021-12-21 18:46:52 -0500

    Added new utility: CSVToTxt
    Convert CSV into ASCII txt

---
+++ Utilities/CSVtoTxt.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+**********************************************************
+* CSVToTxt
+* Convert CSV-formatted learning data into ASCII Txt
+* version: 20211220a
+* By: Nicola Ferralis <feranick@hotmail.com>
+***********************************************************
+'''
+print(__doc__)
+
+import numpy as np
+import sys, os.path
+
+#************************************
+''' Main '''
+#************************************
+class dP:
+    skipRows = 2
+
+def main():
+    try:
+        File =  sys.argv[1]
+        print(" Opening CSV File:"+File+"\n")
+        saveFile(File)
+    except:
+        usage()
+        sys.exit(2)
+
+#************************************
+''' Convert Learning file to HDF5 '''
+#************************************
+def saveFile(File):
+    fileRoot = os.path.splitext(File)[0]
+    try:
+        M = np.loadtxt(open(File, "rb"), delimiter=",", skiprows=dP.skipRows)
+    except:
+        print('\033[1m' + ' Input file not found \n' + '\033[0m')
+        return
+
+    if os.path.isfile(fileRoot+'.txt') is False:
+        with open(fileRoot+'.txt', 'wb') as f:
+            np.savetxt(f, M, delimiter='    ')
+        print(" File: "+File+" converted into ASCII\n")
+    else:
+            print(" File in txt format already exists. Not saving... \n")
+
+#************************************
+''' Lists the program usage '''
+#************************************
+def usage():
+    print(' Usage:\n  python3 CSVToTxt.py <CSV File>.csv\n')
+    p
+    print(' Requires python 3.x. Not compatible with python 2.x\n')
+
+#************************************
+''' Main initialization routine '''
+#************************************
+if __name__ == "__main__":
+    sys.exit(main())
