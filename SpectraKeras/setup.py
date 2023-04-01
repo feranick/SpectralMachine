5af7946eecd42f8d54f73e8144100e68211bd889
Nicola Ferralis <feranick@hotmail.com>
2023-04-01 16:29:39 -0400

    Added support for Python 3.11

--- SpectraKeras/setup.py
+++ SpectraKeras/setup.py
@@ -27,6 +27,7 @@ setup(
      'Programming Language :: Python :: 3.8',
      'Programming Language :: Python :: 3.9',
      'Programming Language :: Python :: 3.10',
+     'Programming Language :: Python :: 3.11',
      'Intended Audience :: Science/Research',
      'Topic :: Scientific/Engineering :: Chemistry',
      'Topic :: Scientific/Engineering :: Physics',
