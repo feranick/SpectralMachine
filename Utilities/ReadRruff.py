2fd650362b569586fdef26b250cf064a7cfd7d23
Nicola Ferralis <feranick@hotmail.com>
2023-05-16 09:28:30 +0200

    Revised file management in ReadRruff

--- Utilities/ReadRruff.py
+++ Utilities/ReadRruff.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 '''
 *********************************************
-*
 * ReadRruFF
 * Convert RRuFFspectra to ASCII
 * File must be in RRuFF
-* version: 20191025a
-*
+* version: 20230516a
 * By: Nicola Ferralis <feranick@hotmail.com>
-*
 ***********************************************
 '''
 print(__doc__)
@@ -31,28 +28,36 @@ def main():
         return
     
     if len(sys.argv) < 3:
-        for ind, f in enumerate(sorted(os.listdir("."))):
-            if (os.path.splitext(f)[-1] == ".txt"):
-                saveFile(sys.argv[1],f)
+        try:
+            os.mkdir(sys.argv[1] + '_ASCII/')
+        except:
+            pass
+        for ind, f in enumerate(os.listdir(sys.argv[1])):
+            root=os.path.splitext(f)
+            if (root[-1] == ".txt") and (root[0][-5:] != "ASCII"):
+                saveFile(sys.argv[1],f,1)
     else:
-        saveFile(sys.argv[1],sys.argv[2])
+        saveFile(sys.argv[1],sys.argv[2],0)
 
     print(' Done!\n')
     
 #************************************
 # Save File
 #************************************
-def saveFile(folder, file):
-    try:
-        with open(file, 'r') as f:
+def saveFile(folder, file, type):
+    #try:
+        with open(folder+'/'+file, 'r') as f:
             M = np.loadtxt(f, skiprows = 10, delimiter = ',', unpack=False)
         print(str(' ' + file) + '\n File OK, converting to ASCII... \n')
-        newFile = folder+os.path.splitext(file)[0] + '_ASCII.txt'
+        if type == 1:
+            newFile = folder + '_ASCII/' + os.path.splitext(file)[0] + '_ASCII.txt'
+        else:
+            newFile = folder + os.path.splitext(file)[0] + '_ASCII.txt'
         with open(newFile, 'ab') as f:
             np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
-    except:
-        print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
-        return
+    #except:
+    #    print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
+    #    return
 
 #************************************
 # Main initialization routine
