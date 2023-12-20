f07e79a58ec84959571177e9ed930e249f8222a7
Nicola Ferralis <feranick@hotmail.com>
2023-12-20 17:55:24 -0500

    AddMixHighFreqMaker.py Add iterations.

--- Utilities/AddMixHighFreqMaker.py
+++ Utilities/AddMixHighFreqMaker.py
@@ -17,13 +17,19 @@ class defParam:
 
 def main():
     if(len(sys.argv)<3):
-        print(' Usage:\n  python3 AddMixHighFreqMaker.py <learnData> <HighFreq threshold>\n')
+        print(' Usage:\n  python3 AddMixHighFreqMaker.py <learnData> <HighFreq threshold>')
+        print('  python3 AddMixHighFreqMaker.py <learnData> <HighFreq threshold> <iterations>\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
-
+        
+    if(len(sys.argv)<4):
+        iter = 1
+    else:
+        iter = int(sys.argv[3])
+    
     M, En, A, Cl = readLearnFile(sys.argv[1])
 
-    newTrainFile = os.path.splitext(sys.argv[1])[0] + '_av-hf' + sys.argv[2]
+    newTrainFile = os.path.splitext(sys.argv[1])[0] + '_av-hf' + sys.argv[2] + '_iter' + str(iter)
 
     if defParam.saveAsTxt == True:
         newTrainFile = newTrainFile + '.txt'
@@ -34,43 +40,49 @@ def main():
         print(" Augmented training file with similar confuiguration exists. Exiting.\n")
         return
 
-    A_new, Cl_new = selectHFdata(A, Cl, int(sys.argv[2]))
-    writeFile(newTrainFile, M, En, A_new, Cl_new)
+    M = selectHFdata(M,int(sys.argv[2]), iter)
+    writeFile(newTrainFile, M)
     print('\n Writing new training file: ', newTrainFile)
-    
     print('\n Done!\n')
     
 #************************************
 ''' Mix high frequency datasets '''
 #************************************
-def selectHFdata(A, Cl, HFthreshold):
-    Cl_new = np.array([])
-    #A_new = np.array([])
+def selectHFdata(M, HFthreshold, iter):
+    print(" Creating new datapoints by averaging members of classes with more than {0:.0f} datapoints...".format(HFthreshold))
+    for h in range(iter):
+        A = M[1:,1:]
+        Cl = M[1:,0]
     
-    uHFCl = np.array([])
-    uniCl = np.unique(Cl).astype(int)
-    for x in enumerate(uniCl):
-        if np.count_nonzero(Cl==uniCl[x[0]]) >= HFthreshold:
-            uHFCl = np.append(uHFCl, uniCl[x[0]])
-    print(" Creating new datapoints by averaging members of the {0:.0f} classes with more than {1:.0f} datapoints...".format(uHFCl.shape[0], HFthreshold))
-    for i in uHFCl:
-        list = np.where(np.in1d(Cl,i) == True)[0]
-        rlist = list
-        np.random.shuffle(rlist)
+        uHFCl = np.array([])
+        uniCl = np.unique(Cl).astype(int)
+        for x in enumerate(uniCl):
+            if np.count_nonzero(Cl==uniCl[x[0]]) >= HFthreshold:
+                uHFCl = np.append(uHFCl, uniCl[x[0]])
         
-        for l in range(list.shape[0]):
-            A_tmp = np.vstack((A[list[l]],A[rlist[l]])).mean(axis=0)
-            try:
-                Cl_new = np.append(Cl_new, i)
-                A_new = np.vstack((A_new,A_tmp))
-            except:
-                Cl_new = np.array([i])
-                A_new = A_tmp
-    
-    print(" Created {0:.0f} new datapoints for each of the {1:.0f} classes with more than {2:.0f} elements.\n".format( Cl_new.shape[0],uHFCl.shape[0],HFthreshold))
-    print(uHFCl)
-          
-    return A_new, Cl_new
+        for i in uHFCl:
+            list = np.where(np.in1d(Cl,i) == True)[0]
+            rlist = list
+            np.random.shuffle(rlist)
+        
+            for l in range(list.shape[0]):
+                A_tmp = np.vstack((A[list[l]],A[rlist[l]])).mean(axis=0)
+                try:
+                    Cl_new = np.append(Cl_new, i)
+                    A_new = np.vstack((A_new,A_tmp))
+                except:
+                    Cl_new = np.array([i])
+                    A_new = A_tmp
+        print('\n Number of original datapoints:', str(M[1:,1:].shape[0]))
+        newMatrix = M
+        temp = np.hstack((Cl_new[np.newaxis].T, A_new))
+        newMatrix = np.vstack((newMatrix, temp))
+        M = newMatrix
+        print(' Number of new datapoints:', str(A_new.shape[0]))
+        print(' Number of total datapoints:', str(newMatrix[1:,1:].shape[0]))
+
+        del A_new, Cl_new, newMatrix
+    return M
 
 #************************************
 ''' Read Learning file '''
@@ -97,21 +109,13 @@ def readLearnFile(learnFile):
 #*****************************************
 ''' Write new learning '''
 #*****************************************
-def writeFile(File, M, En, A, Cl):
-    print('\n Number of original datapoints:', str(M[1:,1:].shape[0]))
-    print(' Number of new datapoints:', str(A.shape[0]))
-    #newMatrix = np.append([0], En).reshape(1,-1)
-    newMatrix = M
-    temp = np.hstack((Cl[np.newaxis].T, A))
-    newMatrix = np.vstack((newMatrix, temp))
-    print(' Number of total datapoints:', str(newMatrix.shape[0]))
-
+def writeFile(File, M):
     if defParam.saveAsTxt == True:
         with open(File, 'ab') as f:
-            np.savetxt(f, newMatrix, delimiter='\t', fmt='%10.6f')
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
     else:
         with h5py.File(File, 'w') as hf:
-            hf.create_dataset("M",  data=newMatrix)
+            hf.create_dataset("M",  data=M)
 
 #************************************
 ''' Main initialization routine '''
