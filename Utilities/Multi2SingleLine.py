2c92b098e296d2aef7fa03c5a4aa763c84c12781
Nicola Ferralis <feranick@hotmail.com>
2024-09-16 13:25:52 -0400

    Multi2SingleLine can take either file or text as input

--- Utilities/Multi2SingleLine.py
+++ Utilities/Multi2SingleLine.py
@@ -4,36 +4,50 @@
 ***************************************************
 * Convert from multiline string into single line
 * By: Nicola Ferralis <feranick@hotmail.com>
-* version v2023.12.15.1
+* version v2024.09.16.1
 ***************************************************
 '''
 print(__doc__)
 
-import sys, os.path
+import sys, os.path, getopt
 
 def main():
 
     saveInText = True
     
     if(len(sys.argv)<2):
-        print(' Usage:\n  python3 Multi2SingleLine.py <file>\n')
+        print(' Usage:\n  python3 Multi2SingleLine.py -t \"<multi-line text>\"')
+        print('  python3 Multi2SingleLine.py -f <file>\n')
         return
     
-    file = sys.argv[1]
-    fileRoot = os.path.splitext(file)[0]
-    outfile = fileRoot+"_single.txt"
+    opts, args = getopt.getopt(sys.argv[1:],
+        "tfh:", ["text", "file", "help"])
     
-    with open(file, 'r') as f:
-        print(" Opening text file with multiple lines:",file,"\n")
-        data = f.read().replace('\n', '')
-        print(" Single line string: \n")
-        print(data,"\n")
+    for o, a in opts:
+        if o in ("-t" , "--text"):
+            string = sys.argv[2]
+            print(string.replace('\n', ''),"\n")
+    
+        if o in ("-f" , "--file"):
+            file = sys.argv[2]
+            fileRoot = os.path.splitext(file)[0]
+            outfile = fileRoot+"_single.txt"
+    
+            with open(file, 'r') as f:
+                print(" Opening text file with multiple lines:",file,"\n")
+                data = f.read().replace('\n', '')
+                print(" Single line string: \n")
+                print(data,"\n")
            
-    if saveInText:
-        with open(outfile, "w") as of:
-            of.write(data)
-            print(" Single line text file saved in:",outfile,"\n")
-
+            if saveInText:
+                with open(outfile, "w") as of:
+                    of.write(data)
+                    print(" Single line text file saved in:",outfile,"\n")
+                    
+        if o in ("-h" , "--help"):
+            print(' Usage:\n  python3 Multi2SingleLine.py <file>\n')
+            return
+            
 #************************************
 ''' Main initialization routine '''
 #************************************
