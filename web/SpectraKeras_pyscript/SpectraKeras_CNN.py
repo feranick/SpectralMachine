b4a4a647fd32f8cad7583402bdd306d549393323
Nicola Ferralis <feranick@hotmail.com>
2024-11-09 00:00:23 -0500

    Added initial version of pyscript versionf SKCNN
    pyscript is still lacking support for tflite_runtime, so this is still a work in progress

---
+++ web/SpectraKeras_pyscript/SpectraKeras_CNN.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+**********************************************
+* SpectraKeras_CNN Classifier and Regressor
+* Pyscript version
+* Only for prediction with tflite_runtime
+* v2024.11.08.1
+* Uses: TFlite_runtime
+* By: Nicola Ferralis <feranick@hotmail.com>
+**********************************************
+'''
+#print(__doc__)
+
+import numpy as np
+import sys, os.path, configparser, js
+from pyscript import fetch, document
+import _pickle as pickle
+#import pickle
+
+from libSpectraKeras import *
+
+baseUrl = "https://gridedgedm.mit.edu/SpectraKeras_pyscript/"
+
+#************************************
+# Parameters
+#************************************
+class Conf():
+    def __init__(self, folder, ini):
+    
+        self.conf = configparser.ConfigParser()
+        self.conf.optionxform = str
+        
+        self.readConfig(ini)
+        self.model_directory = folder+"/"
+        
+        if self.regressor:
+            self.modelName = "model_regressor_CNN.tflite"
+        else:
+            self.predProbThreshold = 90.00
+            self.modelName = "model_classifier_CNN.tflite"
+
+        self.tb_directory = "model_CNN"
+        self.model_name = self.model_directory+self.modelName
+        
+        self.model_le = self.model_directory+"model_le.pkl"
+        self.spectral_range = "model_spectral_range.pkl"
+        
+    def SKDef(self):
+        self.conf['Parameters'] = {
+            'regressor' : False,
+            'normalize' : True,
+            'l_rate' : 0.001,
+            'l_rdecay' : 0.96,
+            'CL_filter' : [1],
+            'CL_size' : [10],
+            'max_pooling' : [20],
+            'dropCNN' : [0],
+            'HL' : [40,70],
+            'dropFCL' : 0,
+            'l2' : 1e-4,
+            'epochs' : 100,
+            'cv_split' : 0.01,
+            'fullSizeBatch' : False,
+            'batch_size' : 64,
+            'numLabels' : 1,
+            'plotWeightsFlag' : False,
+            'plotActivations' : False,
+            'showValidPred' : False,
+            'stopAtBest' : False,
+            'saveBestModel' : False,
+            'metricBestModelR' : 'val_mae',
+            'metricBestModelC' : 'val_accuracy',
+            }
+
+    def sysDef(self):
+        self.conf['System'] = {
+            'kerasVersion' : 2,
+            'makeQuantizedTFlite' : True,
+            'useTFlitePred' : False,
+            'TFliteRuntime' : False,
+            'runCoralEdge' : False,
+            }
+
+    def readConfig(self,configFile):
+        try:
+            self.conf.read_string(configFile)
+        
+            self.SKDef = self.conf['Parameters']
+            self.sysDef = self.conf['System']
+
+            self.regressor = self.conf.getboolean('Parameters','regressor')
+            self.normalize = self.conf.getboolean('Parameters','normalize')
+            self.l_rate = self.conf.getfloat('Parameters','l_rate')
+            self.l_rdecay = self.conf.getfloat('Parameters','l_rdecay')
+            self.CL_filter = eval(self.SKDef['CL_filter'])
+            self.CL_size = eval(self.SKDef['CL_size'])
+            self.max_pooling = eval(self.SKDef['max_pooling'])
+            self.dropCNN = eval(self.SKDef['dropCNN'])
+            self.HL = eval(self.SKDef['HL'])
+            self.dropFCL = self.conf.getfloat('Parameters','dropFCL')
+            self.l2 = self.conf.getfloat('Parameters','l2')
+            self.epochs = self.conf.getint('Parameters','epochs')
+            self.cv_split = self.conf.getfloat('Parameters','cv_split')
+            self.fullSizeBatch = self.conf.getboolean('Parameters','fullSizeBatch')
+            self.batch_size = self.conf.getint('Parameters','batch_size')
+            self.numLabels = self.conf.getint('Parameters','numLabels')
+            self.plotWeightsFlag = self.conf.getboolean('Parameters','plotWeightsFlag')
+            self.plotActivations = self.conf.getboolean('Parameters','plotActivations')
+            self.showValidPred = self.conf.getboolean('Parameters','showValidPred')
+            self.stopAtBest = self.conf.getboolean('Parameters','stopAtBest')
+            self.saveBestModel = self.conf.getboolean('Parameters','saveBestModel')
+            self.metricBestModelR = self.conf.get('Parameters','metricBestModelR')
+            self.metricBestModelC = self.conf.get('Parameters','metricBestModelC')
+            
+            self.kerasVersion = self.conf.getint('System','kerasVersion')
+            self.makeQuantizedTFlite = self.conf.getboolean('System','makeQuantizedTFlite')
+            self.useTFlitePred = self.conf.getboolean('System','useTFlitePred')
+            self.TFliteRuntime = self.conf.getboolean('System','TFliteRuntime')
+            self.runCoralEdge = self.conf.getboolean('System','runCoralEdge')
+        except:
+            print(" Error in reading configuration file. Please check it\n")
+
+async def getFile(folder, file, bin):
+    url = baseUrl+folder+"/"+file
+    if bin:
+        data = await fetch(url).bytearray()
+    else:
+        data = await fetch(url).text()
+    return data
+    
+async def getModel(event):
+    global df, folder
+    document.querySelector("#button").disabled = True
+    document.querySelector("#model").disabled = True
+    
+    if document.querySelector("#model").selectedIndex == 0:
+        folder = "model-raman"
+    else:
+        folder = "model-raman"
+    ini = await getFile(folder, "SpectraKeras_CNN.ini", False)
+    dP = Conf(folder, ini)
+    #modelPkl = await getFile(folder, dP.modelName, True)
+    #df = pickle.loads(modelPkl)
+    #output_div.innerHTML = ""
+    document.querySelector("#button").disabled = False
+    document.querySelector("#model").disabled = False
+
+#************************************
+# Main
+#************************************
+async def main(event):
+    output_div = document.querySelector("#output")
+    output_div.innerHTML = "Please wait..."
+
+    ini = await getFile(folder, "SpectraKeras_CNN.ini", False)
+    dP = Conf(folder, ini)
+    Rtot = np.asarray(js.spectra.to_py())
+    R = Rtot[:,1]
+    Rx = Rtot[:,0]
+    
+    js.console.log(f'{Rtot}')
+    js.console.log(f'{R}')
+    js.console.log(f'{Rx}')
+    
+    output_div.innerHTML = R
+    
+    '''
+    dP = Conf()
+    model = loadModel(dP)
+    with open(dP.spectral_range, "rb") as f:
+        EnN = pickle.load(f)
+
+    R, good = readTestFile(testFile, EnN, dP)
+    if not good:
+        return
+    R = formatForCNN(R)
+
+    if dP.regressor:
+        #predictions = model.predict(R).flatten()[0]
+        predictions, _ = getPredictions(R, model, dP)
+        print('\n  ========================================================')
+        print('  CNN - Regressor - Prediction')
+        print('  ========================================================')
+        predValue = predictions.flatten()[0]
+        print('\n  Predicted value (normalized) = {0:.2f}'.format(predValue))
+        print('  ========================================================\n')
+
+    else:
+        with open(dP.model_le, "rb") as f:
+            le = pickle.load(f)
+        #predictions = model.predict(R, verbose=0)
+        predictions, _ = getPredictions(R, model,dP)
+        pred_class = np.argmax(predictions)
+        if dP.useTFlitePred:
+            predProb = round(100*predictions[0][pred_class]/255,2)
+        else:
+            predProb = round(100*predictions[0][pred_class],2)
+        rosterPred = np.where(predictions[0]>0.1)[0]
+        print('\n  ========================================================')
+        print('  CNN - Classifier - Prediction')
+        print('  ========================================================')
+
+        if dP.numLabels == 1:
+            if pred_class.size >0:
+                predValue = le.inverse_transform(pred_class)[0]
+            else:
+                predValue = 0
+            print('  Prediction\t| Probability [%]')
+            print('  ----------------------------- ')
+            for i in range(len(predictions[0])-1):
+                if predictions[0][i]>0.01:
+                    if dP.useTFlitePred:
+                        print("  {0:d}\t\t| {1:.2f}".format(int(le.inverse_transform(i)[0]),100*predictions[0][i]/255))
+                    else:
+                        print("  {0:d}\t\t| {1:.2f}".format(int(le.inverse_transform(i)[0]),100*predictions[0][i]))
+            print('\n  Predicted value = {0:d} (probability = {1:.2f}%)\n'.format(int(predValue), predProb))
+            print('  ========================================================\n')
+
+        else:
+            print('\n ==========================================')
+            print('\n Predicted value (probability = ' + str(predProb) + '%)')
+            print(' ==========================================\n')
+            print("  1:", str(predValue[0]),"%")
+            print("  2:",str(predValue[1]),"%")
+            print("  3:",str((predValue[1]/0.5)*(100-99.2-.3)),"%\n")
+            print(' ==========================================\n')
+
+    if dP.plotActivations and not dP.useTFlitePred:
+        plotActivationsPredictions(R,model)
+    '''
+
+#************************************
+# Accuracy determination
+#************************************
+def accDeterm(testFile):
+    dP = Conf()
+    model = loadModel(dP)
+    En, A, Cl = readLearnFile(testFile, dP)
+    predictions = np.zeros((0,0))
+    fileName = []
+
+    print("\n  Number of spectra in testing file:",A.shape[0])
+
+    for row in A:
+        R = formatForCNN(np.array([row]))
+        try:
+            predictions, _ = np.vstack((predictions,getPredictions(R, model, dP).flatten()))
+        except:
+            predictions, _ = np.array([getPredictions(R, model,dP).flatten()])
+
+    if dP.regressor:
+        print("\n  Accuracy determination is not defined in regression. Exiting.\n")
+        return
+    else:
+        with open(dP.model_le, "rb") as f:
+            le = pickle.load(f)
+        summaryFile = np.array([['SpectraKeras_CNN','Classifier',''],['Real Class','Predicted Class', 'Probability']])
+
+        successPred = 0
+
+        for i in range(predictions.shape[0]):
+            pred_class = np.argmax(predictions[i])
+            if dP.useTFlitePred:
+                predProb = round(100*predictions[i][pred_class]/255,2)
+            else:
+                predProb = round(100*predictions[i][pred_class],2)
+            rosterPred = np.where(predictions[i][0]>0.1)[0]
+
+            if pred_class.size >0:
+                predValue = le.inverse_transform(pred_class)[0]
+            else:
+                predValue = 0
+            if Cl[i] == predValue:
+                successPred+=1
+
+            summaryFile = np.vstack((summaryFile,[Cl[i], predValue, predProb]))
+
+    print("\n\033[1m  Overall average accuracy: {0:.2f}% \033[0m".format(successPred*100/Cl.shape[0]))
+    summaryFile[0,2] = "Av Acc: {0:.2f}%".format(successPred*100/Cl.shape[0])
+
+    import pandas as pd
+    df = pd.DataFrame(summaryFile)
+    df.to_csv(dP.summaryAccFileName, index=False, header=False)
+    print("\n  Prediction summary saved in:",dP.summaryAccFileName,"\n")
+
+
+#****************************************************
+# Format data for CNN
+#****************************************************
+def formatForCNN(A):
+    listmatrix = []
+    for i in range(A.shape[0]):
+        spectra = np.dstack([A[i]])
+        listmatrix.append(spectra)
+    x = np.stack(listmatrix, axis=0)
+    return x
+
+#************************************
+# Print NN Info
+#************************************
+def printParam():
+    dP = Conf()
+    print('\n  ================================================')
+    print('  \033[1m CNN\033[0m - Parameters')
+    print('  ================================================')
+    print('  Optimizer:','Adam',
+                '\n  Convolutional layers:', dP.CL_filter,
+                '\n  Convolutional layers size:', dP.CL_size,
+                '\n  Max Pooling:', dP.max_pooling,
+                '\n  Dropout CNN:', dP.dropCNN,
+                '\n  Hidden layers:', dP.HL,
+                '\n  Activation function:','relu',
+                '\n  L2:',dP.l2,
+                '\n  Dropout HL:', dP.dropFCL,
+                '\n  Learning rate:', dP.l_rate,
+                '\n  Learning decay rate:', dP.l_rdecay)
+    if dP.fullSizeBatch:
+        print('  Batch size: full')
+    else:
+        print('  Batch size:', dP.batch_size)
+    print('  Epochs:',dP.epochs)
+    print('  Number of labels:', dP.numLabels)
+    print('  Stop at Best Model based on validation:', dP.stopAtBest)
+    print('  Save Best Model based on validation:', dP.saveBestModel)
+    if dP.regressor:
+        print('  Metric for Best Regression Model:', dP.metricBestModelR)
+    else:
+        print('  Metric for Best Classifier Model:', dP.metricBestModelC)
+    #print('  ================================================\n')
---
+++ web/SpectraKeras_pyscript/libSpectraKeras.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+'''
+**********************************************
+* libSpectraKeas - Library for SpectraKeras
+* Pyscript version
+* Only for prediction with tflite_runtime
+* v2024.11.08.1
+* Uses: TFlite_runtime
+* By: Nicola Ferralis <feranick@hotmail.com>
+**********************************************
+'''
+import numpy as np
+import scipy
+import os.path, pickle
+
+#************************************
+# Open Testing Data
+#************************************
+def readTestFile(testFile, En, dP):
+    try:
+        with open(testFile, 'r') as f:
+            #print('\n  Opening sample data for prediction:\n  ',testFile)
+            Rtot = np.loadtxt(f, unpack =True)
+        R = preProcess(Rtot, En, dP)
+    except:
+        print("\033[1m\n File not found or corrupt\033[0m\n")
+        return 0, False
+    return R, True
+
+#****************************************************
+# Check Energy Range and convert to fit training set
+#****************************************************
+def preProcess(Rtot, En, dP):
+    R = np.array([Rtot[1,:]])
+    Rx = np.array([Rtot[0,:]])
+
+    if dP.normalize:
+        norm = Normalizer()
+        R = norm.transform_single(R)
+
+    if(R.shape[1] is not len(En)):
+        print('  Rescaling x-axis from',str(R.shape[1]),'to',str(len(En)))
+        R = np.interp(En, Rx[0], R[0])
+        R = R.reshape(1,-1)
+    return R
+
+#************************************
+# Load saved models
+#************************************
+def loadModel(dP):
+    import tflite_runtime.interpreter as tflite
+    model_name = os.path.splitext(dP.model_name)[0]+'.tflite'
+    model = tflite.Interpreter(model_path=model_name)
+    model.allocate_tensors()
+    print("  Model name:", model_name)
+    return model
+'''
+def loadModel(dP):
+    if dP.TFliteRuntime:
+        import tflite_runtime.interpreter as tflite
+        # model here is intended as interpreter
+        if dP.runCoralEdge:
+            #print(" Running on Coral Edge TPU")
+            try:
+                model_name = os.path.splitext(dP.model_name)[0]+'_edgetpu.tflite'
+                model = tflite.Interpreter(model_path=model_name,
+                    experimental_delegates=[tflite.load_delegate(dP.edgeTPUSharedLib,{})])
+            except:
+                print(" Coral Edge TPU not found. Please make sure it's connected and Tflite-runtime matches the TF version that is installled.")
+        else:
+            model_name = os.path.splitext(dP.model_name)[0]+'.tflite'
+            model = tflite.Interpreter(model_path=model_name)
+        model.allocate_tensors()
+    else:
+        getTFVersion(dP)
+        import tensorflow as tf
+        if checkTFVersion("2.16.0"):
+            import tensorflow.keras as keras
+        else:
+            if dP.kerasVersion == 2:
+                import tf_keras as keras
+            else:
+                import keras
+        if dP.useTFlitePred:
+            # model here is intended as interpreter
+            model_name=os.path.splitext(dP.model_name)[0]+'.tflite'
+            model = tf.lite.Interpreter(model_path=model_name)
+            model.allocate_tensors()
+        else:
+            model_name = dP.model_name
+            if dP.kerasVersion == 2:
+                model = keras.models.load_model(model_name)
+            else:
+                model = keras.saving.load_model(model_name)
+    print("  Model name:", model_name)
+    return model
+'''
+
+#************************************
+# Make prediction based on framework
+#************************************
+def getPredictions(R, model, dP):
+    if dP.useTFlitePred:
+        interpreter = model  #needed to keep consistency with documentation
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        # Test model on random input data.
+        input_shape = input_details[0]['shape']
+        input_data = np.array(R*255, dtype=np.uint8) # Disable this for TF1.x
+        #input_data = np.array(R, dtype=np.float32)  # Enable this for TF2.x (not compatible with on EdgeTPU)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+
+        # The function `get_tensor()` returns a copy of the tensor data.
+        # Use `tensor()` in order to get a pointer to the tensor.
+        predictions = interpreter.get_tensor(output_details[0]['index'])
+    else:
+        predictions = model.predict(R)
+        
+    probabilities = scipy.special.softmax(predictions.astype('double'))
+    return predictions, probabilities
+
+#************************************
+### Create Quantized tflite model
+#************************************
+def makeQuantizedTFmodel(A, dP):
+    pass
+
+#************************************
+# Plot Weights
+#************************************
+def plotWeights(dP, En, A, model, type):
+    pass
+
+#************************************
+# Get TensorFlow Version
+#************************************
+
+def getTFVersion(dP):
+    pass
+    '''
+    import tensorflow as tf
+    if checkTFVersion("2.16.0"):
+        import tensorflow.keras as keras
+        kv = "- Keras v. " + keras.__version__
+    else:
+        if dP.kerasVersion == 2:
+            import tf_keras as keras
+            kv = "- tf_keras v. " + keras.__version__
+        else:
+            import keras
+            kv = "- Keras v. " + keras.__version__
+    from packaging import version
+    if dP.useTFlitePred:
+        print("\n TensorFlow (Lite) v.",tf.version.VERSION,kv, "\n")
+    else:
+        print("\n TensorFlow v.",tf.version.VERSION,kv, "\n" )
+    '''
+        
+def checkTFVersion(vers):
+    return 0
+    '''
+    import tensorflow as tf
+    from packaging import version
+    v = version.parse(tf.__version__)
+    return v < version.parse(vers)
+    '''
+
+#************************************
+# Normalizer
+#************************************
+class Normalizer(object):
+    def __init__(self):
+        self.YnormTo = 1
+        print("  Normalizing spectra between 0 and 1")
+
+    def transform_matrix(self,y):
+        yn = np.copy(y)
+        for i in range(0,y.shape[0]):
+            if np.amax(y[i,:]) - np.amin(y[i,:]) == 0:
+                pass
+            else:
+                yn[i,:] = np.multiply(y[i,:] - np.amin(y[i,:]),
+                    self.YnormTo/(np.amax(y[i,:]) - np.amin(y[i,:])))
+        return yn
+
+    def transform_single(self,y):
+        yn = np.copy(y)
+        yn = np.multiply(y - np.amin(y),
+                self.YnormTo/(np.amax(y) - np.amin(y)))
+        return yn
+
+    def save(self, name):
+        with open(name, 'ab') as f:
+            pickle.dump(self, f)
+
+#************************************
+# Normalize Label
+#************************************
+class NormalizeLabel(object):
+    def __init__(self, M, dP):
+        self.M = M
+        self.normalizeLabel = dP.normalizeLabel
+        self.useGeneralNormLabel = dP.useGeneralNormLabel
+        self.useCustomRound = dP.useCustomRound
+        self.minGeneralLabel = dP.minGeneralLabel
+        self.maxGeneralLabel = dP.maxGeneralLabel
+        self.YnormTo = dP.YnormTo
+        self.stepNormLabel = dP.stepNormLabel
+
+        self.data = np.arange(0,1,self.stepNormLabel)
+        self.min = np.zeros([self.M.shape[1]])
+        self.max = np.zeros([self.M.shape[1]])
+
+        if self.normalizeLabel:
+            if self.useGeneralNormLabel:
+                self.min[0] = dP.minGeneralLabel
+                self.max[0] = dP.maxGeneralLabel
+            else:
+                self.min[0] = np.amin(self.M[1:,0])
+                self.max[0] = np.amax(self.M[1:,0])
+
+        for i in range(1,M.shape[1]):
+            self.min[i] = np.amin(self.M[1:,i])
+            self.max[i] = np.amax(self.M[1:,i])
+
+    def transform_matrix(self,y):
+        Mn = np.copy(y)
+        if self.normalizeLabel:
+            Mn[1:,0] = np.multiply(y[1:,0] - self.min[0],
+                self.YnormTo/(self.max[0] - self.min[0]))
+            if self.useCustomRound:
+                customData = CustomRound(self.data)
+                for i in range(1,y.shape[0]):
+                    Mn[i,0] = customData(Mn[i,0])
+
+        for i in range(1,y.shape[1]):
+            Mn[1:,i] = np.multiply(y[1:,i] - self.min[i],
+                self.YnormTo/(self.max[i] - self.min[i]))
+        return Mn
+
+    def transform_valid(self,V):
+        Vn = np.copy(V)
+        for i in range(0,V.shape[0]):
+            Vn[i,1] = np.multiply(V[i,1] - self.min[i+1],
+                self.YnormTo/(self.max[i+1] - self.min[i+1]))
+        return Vn
+
+    def transform_inverse_single(self,v):
+        vn = self.min[0] + v*(self.max[0] - self.min[0])/self.YnormTo
+        return vn
+
+    def save(self, name):
+        with open(name, 'ab') as f:
+            pickle.dump(self, f)
+
+#************************************
+# CustomRound
+#************************************
+class CustomRound:
+    def __init__(self,iterable):
+        self.data = sorted(iterable)
+
+    def __call__(self,x):
+        data = self.data
+        ndata = len(data)
+        from bisect import bisect_left
+        idx = bisect_left(data,x)
+        if idx <= 0:
+            return data[0]
+        elif idx >= ndata:
+            return data[ndata-1]
+        x0 = data[idx-1]
+        x1 = data[idx]
+        if abs(x-x0) < abs(x-x1):
+            return x0
+        return x1
+
+#************************************
+# MultiClassReductor
+#************************************
+class MultiClassReductor():
+    def __self__(self):
+        self.name = name
+
+    def fit(self,tc):
+        self.totalClass = tc.tolist()
+
+    def transform(self,y):
+        Cl = np.zeros(y.shape[0])
+        for j in range(len(y)):
+            Cl[j] = self.totalClass.index(np.array(y[j]).tolist())
+        return Cl
+
+    def inverse_transform(self,a):
+        return [self.totalClass[int(a)]]
+
+    def classes_(self):
+        return self.totalClass
