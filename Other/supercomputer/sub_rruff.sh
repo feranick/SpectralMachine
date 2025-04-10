1c201f0b6d6718f8e9d9a8dd6ab0f4939f3e45e4
Nicola Ferralis <feranick@hotmail.com>
2025-04-10 17:14:07 -0400

    Added supercomputer script.

---
+++ Other/supercomputer/sub_rruff.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+#SBATCH -C gpu
+#SBATCH -t 18:00:00
+#SBATCH -J $1
+#SBATCH -o $1.o%j
+#SBATCH -A m526
+#SBATCH -N 1
+#SBATCH -c 32
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=none
+#SBATCH -q regular
+#SBATCH --mail-user=ferralis@mit.edu
+#SBATCH --mail-type=ALL
+
+exe=/global/homes/f/feranick/ml/train_rruff.sh
+input="20250406"
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+command="srun -n 4 --cpu-bind=cores --gpu-bind=none train_rruff.sh $1"
+
+echo $command
+
+$command
