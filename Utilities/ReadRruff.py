243fd7ea7afa420832d582e6e5cbb1d01ce8f9e3
Nicola Ferralis <feranick@hotmail.com>
2025-04-14 15:07:26 -0400

    Rewritten Rruff and xmu readers.

--- Utilities/ReadRruff.py
+++ Utilities/ReadRruff.py
@@ -5,7 +5,7 @@
 * ReadRruFF
 * Convert RRuFFspectra to ASCII
 * File must be in RRuFF
-* version: v2025.04.13.1
+* version: v2025.04.14.1
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************
 '''
@@ -18,47 +18,43 @@ import sys, os.path
 # Main
 #************************************
 def main():
-    
-    if len(sys.argv) == 3:
-        saveFile(sys.argv[1],sys.argv[2],0)
-    elif len(sys.argv) == 2:
-        try:
-            os.mkdir(sys.argv[1] + '_ASCII/')
-        except:
-            pass
-        for ind, f in enumerate(os.listdir(sys.argv[1])):
-            root=os.path.splitext(f)
-            filepath = os.path.join(sys.argv[1], f)
-            if os.path.isfile(filepath) and (root[-1] == ".txt") and (root[0][-5:] != "ASCII"):
-                saveFile(sys.argv[1],f,1)
-    elif len(sys.argv) < 2 or if len(sys.argv) > 3:
+    if len(sys.argv) == 2:
+        if os.path.isfile(sys.argv[1]):
+            saveFile(sys.argv[1],"",0)
+        else:
+            asciiPath = os.path.join(sys.argv[1], 'ASCII')
+            if not os.path.isdir(asciiPath):
+                os.mkdir(asciiPath)
+            for ind, f in enumerate(os.listdir(sys.argv[1])):
+                root=os.path.splitext(f)
+                filePath = os.path.join(sys.argv[1], f)
+                if os.path.isfile(filePath) and (root[-1] == ".txt") and (root[0][-5:] != "ASCII"):
+                    saveFile(f,asciiPath,1)
+    else:
         print(' Usage:\n')
-        print(' (Single File): python3 ReadRruff.py <folder> <RRuFF filename>\n')
+        print(' (Single File): python3 ReadRruff.py <RRuFF filename>\n')
         print(' (Batch conversion): python3 ReadRruff.py <folder>\n')
         print(' <folder> Directory where original files are located\n')
         print(' Requires python 3.x. Not compatible with python 2.x\n')
         return
     
-        
-
     print(' Done!\n')
     
 #************************************
 # Save File
 #************************************
-def saveFile(folder, file, type):
+def saveFile(file, folder, type):
     try:
-        with open(folder+'/'+file, 'r') as f:
+        with open(file, 'r') as f:
             M = np.loadtxt(f, skiprows = 10, delimiter = ',', unpack=False)
         print(str(' ' + file) + '\n File OK, converting to ASCII... \n')
-        if type == 1:
-            newFile = folder + '_ASCII/' + os.path.splitext(file)[0] + '_ASCII.txt'
-        else:
-            newFile = folder + '/' + os.path.splitext(file)[0] + '_ASCII.txt'
+    
+        newFile = os.path.join(folder, os.path.splitext(file)[0] + '_ASCII.txt')
+    
         with open(newFile, 'w') as f:
             np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
     except:
-        print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
+        print('\033[1m ' + str(sys.argv[1]) + ' file or folder not found \n' + '\033[0m')
         return
 
 #************************************
--- Utilities/ReadXmu.py
+++ Utilities/ReadXmu.py
@@ -2,45 +2,64 @@
 # -*- coding: utf-8 -*-
 '''
 ***********************************************
-* ReadXmu
+* ReadRruFF
 * Convert Xanes Xmu to ASCII
-* File must be in xmu format
-* version: v2023.12.15.1
+* File must be in xmu fomat
+* version: v2025.04.14.1
 * By: Nicola Ferralis <feranick@hotmail.com>
 ***********************************************
 '''
 print(__doc__)
 
-
 import numpy as np
-import sys, os.path, getopt, glob, csv
-
-def main():
-    if(len(sys.argv)<2):
-        print(' Usage:\n  python3 readxmu.py <Xmu filename>\n')
-        print(' Requires python 3.x. Not compatible with python 2.x\n')
-        return
-
-    try:
-        with open(sys.argv[1], 'r') as f:
-            #M = np.loadtxt(f, skiprows = 40, unpack=True)
-            M = np.loadtxt(f, unpack=True)
-        print(str(' ' + sys.argv[1]) + '\n File OK, converting to ASCII... \n')
-    except:
-        print('\033[1m ' + str(sys.argv[1]) + ' file not found \n' + '\033[0m')
-        return
-
-    print(M[0])
-
-    newFile = os.path.splitext(sys.argv[1])[0] + '_b.txt'
-
-    with open(newFile, 'ab') as f:
-        np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
-
-    print(' Done!\n')
+import sys, os.path
 
 #************************************
-''' Main initialization routine '''
+# Main
+#************************************
+def main():
+    if len(sys.argv) == 2:
+        if os.path.isfile(sys.argv[1]):
+            saveFile(sys.argv[1],"",0)
+        else:
+            asciiPath = os.path.join(sys.argv[1], 'ASCII')
+            if not os.path.isdir(asciiPath):
+                os.mkdir(asciiPath)
+            for ind, f in enumerate(os.listdir(sys.argv[1])):
+                root=os.path.splitext(f)
+                filePath = os.path.join(sys.argv[1], f)
+                if os.path.isfile(filePath) and (root[-1] == ".txt") and (root[0][-5:] != "ASCII"):
+                    saveFile(f,asciiPath,1)
+    else:
+        print(' Usage:\n')
+        print(' (Single File): python3 ReadXmu.py <xmu filename>\n')
+        print(' (Batch conversion): python3 ReadXmu.py <folder>\n')
+        print(' <folder> Directory where original files are located\n')
+        print(' Requires python 3.x. Not compatible with python 2.x\n')
+        return
+    
+    print(' Done!\n')
+    
+#************************************
+# Save File
+#************************************
+def saveFile(file, folder, type):
+    try:
+        with open(file, 'r') as f:
+            #M = np.loadtxt(f, skiprows = 40, unpack=True)
+            M = np.loadtxt(f, unpack=True)
+        print(str(' ' + file) + '\n File OK, converting to ASCII... \n')
+    
+        newFile = os.path.join(folder, os.path.splitext(file)[0] + '_ASCII.txt')
+    
+        with open(newFile, 'w') as f:
+            np.savetxt(f, M, delimiter='\t', fmt='%10.6f')
+    except:
+        print('\033[1m ' + str(sys.argv[1]) + ' file or folder not found \n' + '\033[0m')
+        return
+
+#************************************
+# Main initialization routine
 #************************************
 if __name__ == "__main__":
     sys.exit(main())
