35ebd89ebd8831a2f517131d888a5943b809fb5d
Nicola Ferralis <feranick@hotmail.com>
2025-04-14 10:31:51 -0400

    Updatd index.php

--- web/SpectraKeras-web/index.php
+++ web/SpectraKeras-web/index.php
@@ -106,8 +106,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <br>Files are discarded after prediction. <a href="https://github.com/feranick/SpectralMachine" target="_blank" rel="noopener noreferrer">SpectraKeras is open-source and code and python scripts are available on Github</a>.
     <br><br> A sample input file can be found <a href="ml-raman/Abelsonite.txt">here</a>.
     <br><br>To get names of the minerals corresponding to the prediction values, use the link to the ML models below.
-    <br>Current Raman ML model: <a href="ml-raman/AAA-20241110_2024-11-13_17-33-04.csv">AAA-20241110_norm1_train-cv_hfsel20_val37 CNN_2-15_b4_keras3 </a>
-    <br>Current XRD ML model: <a href="ml-xrd/AAA-powder-20241110s2_2024-11-13_18-24-29.csv">AAA-Powder_20241110s2_norm1_train-cv_hfsel10_val22 CNN_powder_2-15_b4</a>
+    <br>Current Raman ML model: <a href="ml-raman/AAA-20250406r_2025-04-14_00-52-20.csv">AAA-20250406_norm1_train-cv_hfsel20_val37 CNN_2-15_b4_keras3 </a>
+    <br>Current XRD ML model: <a href="ml-xrd/AAA-powder-20250225_2025-02-25_16-07-01.csv">AAA-Powder_20250225_norm1_train-cv_hfsel10_val22 CNN_powder_2-15_b4</a>
     
     <form name="SpectraKeras" method="post" enctype="multipart/form-data">
       <br><br><input type="file" name="files[]" multiple />
